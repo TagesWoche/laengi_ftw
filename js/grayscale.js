9c6ca87f9d8b4a2c7749650f60e51fbea2beac62
Felix Michel <felix_michel@gmx.ch>
2017-03-23 13:43:09 +0100

    better placements of anchors

--- js/grayscale.js
+++ js/grayscale.js
@@ -104,7 +104,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        - 50 }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
